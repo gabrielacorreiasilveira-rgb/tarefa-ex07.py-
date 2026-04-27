baa45f799d33159cb31e8d53d152050de07a9b7e
gabriela silveira <gabriela.correia.silveira@escola.pr.gov.br>
2026-04-27 10:46:58 -0300

    atividades 7 ao 17 python gabriela

---
+++ ex17.py
@@ -0,0 +1,9 @@
+#ex 17 - Login simples
+usuario = input("Usuário: ")
+senha = input("Sistema: ")
+
+
+if usuario == "admin" and senha == "123":
+    print("Login realizado")
+else:
+    print("Usuário ou senha incorretos")
